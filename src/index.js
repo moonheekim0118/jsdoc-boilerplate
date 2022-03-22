b86c94844d7009815feddb1efe2bd63ebb3fee80
moonheekim0118 <hellomooneekim@gmail.com>
2022-03-22 12:11:20 +0900

    feat: 설계 작성

--- src/index.js
+++ src/index.js
@@ -1,51 +1,27 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * 자판기 클래스
+ * @class
+ * @public
  */
-function Book(title, author) {}
-
-/**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
- */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
-
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
-
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
-
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+class VendingMachine {
+  constructor() {
+    /** @member {Map<Number, Object>} */
+    this.menu = new Map();
+  }
+  /**
+   * 메뉴를 추가한다.
+   * @constructor
+   * @param {string} menuName - 추가 할 메뉴 이름
+   * @param {number} menuCount - 추가 할 메뉴 개수
+   * @param {number} menuPrice - 추가 할 메뉴 가격
+   * @return {string} - 추가 된 메뉴 정보
+   */
+  setItem(menuName, menuCount, menuPrice) {}
+  /**
+   * 메뉴를 구매한다.
+   * @constructor
+   * @param {number} menuPrice - 구매 할 메뉴 가격
+   * @return {string} - 구매 한 메뉴 정보
+   */
+  getItem(menuPrice) {}
+}
